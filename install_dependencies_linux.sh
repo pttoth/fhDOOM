ea5b0b74f7d6cb4f29f330079a14d7dd9522a192
Peter Toth <peter.t.toth92@gmail.com>
2021-04-10 22:56:27 +0200

    added missing shebang for dependency installer script

--- install_dependencies_linux.sh
+++ install_dependencies_linux.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 sudo apt install libc-dev libc++-dev libc++abi-dev clang gcc-multilib g++-multilib libasound2-dev -y
 sudo apt install libx11-dev:i386 libxext-dev:i386 libxxf86vm-dev:i386 libalut-dev:i386 libgl1-mesa-dev:i386 -y
